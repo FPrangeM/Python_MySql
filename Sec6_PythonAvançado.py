03faf800735512b00a97570800089d95391245de
FPrangeM <fpmenechini@gmail.com>
2023-05-26 11:37:46 -0300

    Função Map

--- Sec6_PythonAvançado.py
+++ Sec6_PythonAvançado.py
@@ -1,5 +1,6 @@
 
-# Lidando com List Comprehension
+# List Comprehension
+# Maneira de simplificar for loops e criar listas em uma unica linha
 
 x = [1, 2, 3, 4, 5]
 y = [i**2 for i in x]
@@ -10,14 +11,27 @@ print(y)
 print(z)
 
 
-# Trabalhando com a função filter
+# Função filter
+# Interpreta uma função para cada valor de uma lista e retorna os itens com resposta da fução
 
 def epar(i):
     if i % 2 == 0:
-        return i
+        return True
 
 
 lista = list(range(10))
 
 lista_par = list(filter(epar, lista))
 print(lista_par)
+
+
+# Função Map
+# Aplica uma função a cada valor de uma lista
+
+def sub_2(x):
+    return x-2
+
+
+lista = list(range(1, 20, 3))
+print(lista)
+print(list(map(sub_2, lista)))
